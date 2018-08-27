caff88d9317f2c801044826b23c243f503f065f0
João Miguel <joaomigue.farias@gmail.com>
2018-08-27 20:34:42 -0300

    Ajustes para PWA
    2.5

--- sw.js
+++ sw.js
@@ -1,43 +1,68 @@
-//This is the "Offline copy of pages" service worker
+// esse é o nome que seu cache terá na caches API
+var cacheName = 'nome_do_meu_app.v1.0.2';
 
-//Install stage sets up the index page (home page) in the cache and opens a new cache
-self.addEventListener('install', function (event) {
-  var indexPage = new Request('index.html');
-  event.waitUntil(
-    fetch(indexPage).then(function(response) {
-      return caches.open('pwabuilder-offline').then(function(cache) {
-        console.log('[PWA Builder] Cached index page during Install'+ response.url);
-        return cache.put(indexPage, response);
-      });
-  }));
+// aqui você escolhe quais arquivos serão cacheados,
+//  basicamente todos os arquivos podem ser cacheados
+var filesToCache = [
+  '/',
+];
+
+// evento de INSTALAÇÃO do service worker,
+//  nós só conseguimos salvar arquivos em cache
+//  após o service worker ter sido instalado,
+//  ou seja, após não acontecer nenhum problema.
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName)
+      .then(function(cache) {
+        // adiciona os arquivos em cache
+        return cache.addAll(filesToCache);
+      })
+  );
 });
 
-//If any fetch fails, it will look for the request in the cache and serve it from there first
-self.addEventListener('fetch', function(event) {
-  var updateCache = function(request){
-    return caches.open('pwabuilder-offline').then(function (cache) {
-      return fetch(request).then(function (response) {
-        console.log('[PWA Builder] add page to offline'+response.url)
-        return cache.put(request, response);
-      });
-    });
-  };
-
-  event.waitUntil(updateCache(event.request));
-
-  event.respondWith(
-    fetch(event.request).catch(function(error) {
-      console.log( '[PWA Builder] Network request Failed. Serving content from cache: ' + error );
-
-      //Check to see if you have it in the cache
-      //Return response
-      //If not in the cache, then return error page
-      return caches.open('pwabuilder-offline').then(function (cache) {
-        return cache.match(event.request).then(function (matching) {
-          var report =  !matching || matching.status == 404?Promise.reject('no-match'): matching;
-          return report
-        });
-      });
-    })
+// evento de ATIVAÇÃO do service worker,
+//  esse evento é chamado quando seu service worker
+//  ja foi instalado previamente, ou seja, não é a 
+//  primeira vez que o seu usuário entra em seu site.
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys()
+      .then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          // para manter nosso cache sempre atualizado e
+          //  sem arquivos desnecessários/versões antigas, 
+          //  o indicado é verificarmos se no cache do nosso
+          //  service worker, as versões que estão lá são
+          //  diferentes das atuais, caso seja nós deletamos
+          //  a antiga para deixar somente a versão mais atual.
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        }));
+      })
   );
-})
+  // a linha abaixo é para resolver um problema
+  //  do service worker não retornar as informações
+  //  mais recentes da aplicação no momento que seu 
+  //  usuário acessa a página, issso acontece por que
+  //  seu service worker ainda não foi ativado, dessa forma
+  //  nós fazemos ele ativar mais rápido com as informações
+  //  mais novas que ele possuir.
+  return self.clients.claim();
+});
+
+// evento de FETCHING do service worker,
+//  ele verificará quando alguma requisição corresponde
+//  a algum arquivo que ja está no cache,
+//  caso esteja ele retornará o arquivo do cache,
+//  se não estiver, fará a requisição do arquivo
+//  e retornará o resultado dessa requisição.
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request)
+      .then(function(response) {
+        return response || fetch(e.request);
+      })
+  );
+});
