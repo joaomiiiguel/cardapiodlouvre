7138afc7f140c99fcb43b77089e10fa64c2daaa4
João Miguel <joaomigue.farias@gmail.com>
2018-08-27 12:04:41 -0300

    Ajustando o Service Worker

---
+++ sw.js
@@ -0,0 +1,56 @@
+---
+layout: null
+---
+
+const staticCacheName = 'cardapio-dlouvre-2018-27-08-14-19';
+
+const filesToCache = [
+  { % for page in site.pages_to_cache % }
+    '{ { page } }',
+  { % endfor % }
+  { % for post in site.posts limit: 6 % }
+    '{ { post.url } }',
+  { % endfor % }
+  { % for asset in site.files_to_cache % }
+    '{ { asset } }',
+  { % endfor % }
+];
+
+// Cache on install
+this.addEventListener("install", event => {
+  this.skipWaiting();
+
+  event.waitUntil(
+    caches.open(staticCacheName)
+      .then(cache => {
+        return cache.addAll(filesToCache);
+    })
+  )
+});
+
+// Clear cache on activate
+this.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(cacheName => (cacheName.startsWith('cardapio-dlouvre-')))
+          .filter(cacheName => (cacheName !== staticCacheName))
+          .map(cacheName => caches.delete(cacheName))
+      );
+    })
+  );
+});
+
+// Serve from Cache
+this.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+      .catch(() => {
+        return caches.match('/offline/index.html');
+      })
+  )
+});
